8379ce0979793bf0bbfa922eff2746604ec5276f
Jose Gonzalez <joseantonio1389.jag@gmail.com>
2019-07-30 16:16:53 -0600

    Termianada la clase 16

--- clase16.js
+++ clase16.js
@@ -9,19 +9,26 @@ var jose ={
 
 console.log(`Al inicio ${jose.name} pesa ${jose.peso}kg`)
 
-const INCREMENTO_PESO = 0.2;
+const INCREMENTO_PESO = 0.3;
 const DIAZ_DEL_ANIO =365
 
 const aumentaDePeso = persona => persona.peso += INCREMENTO_PESO;
 const pierdePeso = persona => persona.peso -= INCREMENTO_PESO;
+const comeMuchp = () => Math.random() < 0.3;
+const realizaDeporte = () => Math.random() < 0.4;
 
-for (var i = 1; i <= DIAZ_DEL_ANIO; i++) {
-    var random = Math.random()
-    if(random < 0.25){
+const META = jose.peso - 3;
+var dias = 0;
+
+while(jose.peso > META ){
+    //debugger
+    if(comeMuchp()){
         aumentaDePeso(jose);
-    }else if(random < 0.5){
+    }
+    if(realizaDeporte){
         pierdePeso(jose);
     }
+    dias += 1
 }
 
-console.log(`Al fianl del año ${jose.name} pesa ${jose.peso.toFixed(1)}kg`)
+console.log(`Pasaron ${dias} dias hasta que ${jose.name} adelgazo a ${jose.peso.toFixed(1)}kg`)
